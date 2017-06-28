16a7ffd8aa0cc392ad1549f3fad162e1f75ae0c1
SARA SIMON <smbsimon@gmail.com>
2017-06-28 10:53:38 -0400

    i give up

--- app/query.py
+++ app/query.py
@@ -2,20 +2,16 @@
 import feedparser
 
 def get_timestamp():
-    try:
-        latest_newscast_feed = "https://podcasts.vpr.net/newscasts"
-        parsed_feed = feedparser.parse(latest_newscast_feed)
-        parsed_title = parsed_feed["items"][0]["title"]
-    except IndexError:
-         pass
+    latest_newscast_feed = "https://podcasts.vpr.net/newscasts"
+    parsed_feed = feedparser.parse(latest_newscast_feed)
+    parsed_title = parsed_feed["items"][0]["title"]
+    
     return parsed_title
 
 
 def get_latest_episode():
-    try:
-        latest_newscast_feed = "https://podcasts.vpr.net/newscasts"
-        parsed_feed = feedparser.parse(latest_newscast_feed)
-        parsed_audio = parsed_feed["items"][0]["enclosures"]
-    except IndexError:
-        pass
+    latest_newscast_feed = "https://podcasts.vpr.net/newscasts"
+    parsed_feed = feedparser.parse(latest_newscast_feed)
+    parsed_audio = parsed_feed["items"][0]["enclosures"]
+
     return parsed_audio[0]["href"]
